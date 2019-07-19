be3e487d2ff75f734e2217120e0ecf1839b91a9f
Tiruvengadam Lakshmanan <t.lakshmanan@perficient.com>
2019-07-19 15:23:47 +0530

    added pagenumber footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ class App extends Component {
       .then((canvas) => {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.addImage(imgData, 'JPEG', 17, 17);
+        const pageCount = pdf.internal.getNumberOfPages();
+        for (var i = 1; i <= pageCount; i++) {
+          pdf.text(String(i), 196, 285);
+        }
         pdf.save("download.pdf");
       })
       ;
@@ -24,9 +28,9 @@ class App extends Component {
         <div className="mb5">
           <button onClick={this.printDocument}>Print</button>
         </div>
-        <div id="divToPrint" className="row">
+        <div id="divToPrint">
           
-            <div className='col-7'>
+            
               <h3>10 Great and Easy English Books You Must Read</h3>
               <h5>Reading is rewarding. </h5>
 
@@ -38,8 +42,8 @@ class App extends Component {
 
               <p>Being able to read a novel in another language and understand it is a huge achievement.
                    <br /> You’ll feel accomplished the moment you read that final page,</p>
-            </div>
-            <div className='col-6'>
+            
+            
               <Barcode value='https://www.google.com/'
                 displayValue='true'
                 format="CODE128"
@@ -54,7 +58,7 @@ class App extends Component {
                 margin='30'
                 width='2'
               />
-            </div>
+           
           </div>
         
       </div>
