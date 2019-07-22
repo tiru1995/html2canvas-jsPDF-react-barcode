66061961d3124c851816c9dd56f0bacf9b334b9c
Tiruvengadam Lakshmanan <t.lakshmanan@perficient.com>
2019-07-22 18:53:18 +0530

    updated with header

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import Barcode from 'react-barcode';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.myRef = React.createRef();
-    this.printDocument=this.printDocument.bind(this)
+    this.printDocument = this.printDocument.bind(this)
   }
 
   printDocument() {
-    const input = this.myRef.current  //document.getElementById('divToPrint');
+    const pdf = new jsPDF();
+    //document.getElementById('divToPrint');
+    pdf.text('Header Of the page', 10, 9);
+    //pdf.text(50, 40, "Header");
+
+
+    const input = this.myRef.current
     html2canvas(input)
       .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 17, 17);
+       const imgData = canvas.toDataURL('image/png');
+       pdf.addImage(imgData, 'JPEG', 17, 17);
         const pageCount = pdf.internal.getNumberOfPages();
         for (var i = 1; i <= pageCount; i++) {
           pdf.text(String(i), 196, 285);
@@ -34,38 +39,38 @@ class App extends Component {
           <button onClick={this.printDocument}>Print</button>
         </div>
         <div id="divToPrint" ref={this.myRef}>
-          
-            
-              <h3>10 Great and Easy English Books You Must Read</h3>
-              <h5>Reading is rewarding. </h5>
 
-              First name:<br />
-              <input type="text" name="firstname" />
-              <br />
-              Last name:<br />
-              <input type="text" name="lastname" />
 
-              <p>Being able to read a novel in another language and understand it is a huge achievement.
+          <h3>10 Great and Easy English Books You Must Read</h3>
+          <h5>Reading is rewarding. </h5>
+
+          First name:<br />
+          <input type="text" name="firstname" />
+          <br />
+          Last name:<br />
+          <input type="text" name="lastname" />
+
+          <p>Being able to read a novel in another language and understand it is a huge achievement.
                    <br /> You’ll feel accomplished the moment you read that final page,</p>
-            
-            
-              <Barcode value='https://www.google.com/'
-                displayValue='true'
-                format="CODE128"
-                margin='30'
-                width='1'
-              />
 
-              <br />
-              <Barcode value='https://www.ibaset.com/'
-                displayValue='false'
-                format="CODE128"
-                margin='30'
-                width='2'
-              />
-           
-          </div>
-        
+
+          <Barcode value='https://www.google.com/'
+            displayValue='true'
+            format="CODE128"
+            margin='30'
+            width='1'
+          />
+
+          <br />
+          <Barcode value='https://www.ibaset.com/'
+            displayValue='false'
+            format="CODE128"
+            margin='30'
+            width='2'
+          />
+
+        </div>
+
       </div>
     )
   }
